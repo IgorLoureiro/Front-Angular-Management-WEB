5216e8406102a8f8fec1051af6bdc5f96c52f196
IgorL <igor.loureiro@mobilus.com.br>
2025-02-04 22:41:00 -0300

    Add markers to the map

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Output, EventEmitter } from '@angular/core';
+import L from 'leaflet';
 
 @Component({
   selector: 'app-navbar',
@@ -11,11 +12,17 @@ export class NavbarComponent {
 
   UserImage: string = "assets/veterinarian.png";
   Username: string = "UsuarioTeste";
+  @Output() addingPointer = new EventEmitter<boolean>();
+  AddingPointer: boolean = false;
 
-  constructor(){
+  constructor(private el: ElementRef){
 
   }
 
-  
+  AddAPointer(){
+    document.body.style.cursor = 'url("https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png") 16 32, auto';
+    this.AddingPointer = true;
+    this.addingPointer.emit(this.AddingPointer);
+  }
 
 }
--- src/app/paginas/tela-principal/tela-principal.component.ts
+++ src/app/paginas/tela-principal/tela-principal.component.ts
@@ -1,53 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, Input, PLATFORM_ID } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-declare const L: any;
+import * as L from 'leaflet';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-tela-principal',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './tela-principal.component.html',
   styleUrl: './tela-principal.component.css'
 })
 export class TelaPrincipalComponent {
 
-userLatitude!: number
-userLongitude!: number
-error!: string
+  private map!: any;
+  userLatitude!: number;
+  userLongitude!: number;
+  error!: string;
+  isAddingPointer: boolean = false;
+  isBrowser: boolean;
+  showModal: boolean = true;
 
-
-ngOnInit() {
-  if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.userLatitude = position.coords.latitude;
-        this.userLongitude = position.coords.longitude;
-        console.log(this.userLatitude)
-        console.log(this.userLongitude)
-        console.log(position.coords.accuracy)
-
-        let map = L.map('map').setView([this.userLatitude, this.userLongitude], 13);
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-        var marker = L.marker([this.userLatitude, this.userLongitude]).addTo(map);
-        marker.bindPopup("<b>Você está aqui</b><br>Sua localização").openPopup();
-      },
-      (err) => {
-        this.error = 'Erro ao obter localização: ' + err.message;
-      },
-      { enableHighAccuracy: true,
-        timeout: 30000,          
-        maximumAge: 0
-       }
-    );
-  } else {
-    this.error = 'Geolocalização não é suportada pelo navegador.';
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
-}
 
+  async ngOnInit() {
+    if (this.isBrowser) {
+      const L = await import('leaflet');
+
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.userLatitude = position.coords.latitude;
+            this.userLongitude = position.coords.longitude;
+
+            this.map = L.map('map', {
+              dragging: true,
+              doubleClickZoom: true,
+            }).setView([this.userLatitude, this.userLongitude], 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
+
+            var marker = L.marker([this.userLatitude, this.userLongitude]).addTo(this.map);
+            marker.bindPopup("<b>Você está aqui</b><br>Sua localização").openPopup();
+          },
+          (err) => {
+            this.error = 'Erro ao obter localização: ' + err.message;
+          },
+          { enableHighAccuracy: true, timeout: 30000, maximumAge: 0 }
+        );
+      } else {
+        this.error = 'Geolocalização não é suportada pelo navegador.';
+      }
+    }
+  }
+
+  onAddingPointer($event: any) {
+    if($event == true){
+      this.map.dragging.disable();
+      this.isAddingPointer = true;
+    } else{
+      this.map.dragging.enable();
+      this.isAddingPointer = false;
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  addPointerInMap(event: MouseEvent){
+    this.map.on('click', async (event: any) => {
+      if(this.isAddingPointer){
+        const L = await import('leaflet');
+        const lat = event.latlng.lat;
+        const lng = event.latlng.lng;
+
+        this.openModal()
+
+        // var marker = L.marker([lat, lng]).addTo(this.map);
+        // marker.bindPopup("<b>Você clicou aqui</b>").openPopup();
+
+        // document.body.style.cursor = 'default';
+        // this.map.dragging.enable();
+        // this.isAddingPointer = false;
+      }
+    });
+  }
+
+  openModal(){
+    if (this.isBrowser) {
+      const modalElement = document.getElementById('staticBackdrop');
+      if (modalElement) {
+        const modal = new bootstrap.Modal(modalElement!);
+        modal.show();
+      }
+    }
+  }
 
 }
